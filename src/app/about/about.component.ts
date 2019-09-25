63e93e42b122f233e0d9cdef5e1c28243b379496
Sam Kavanagh <hganavak@gmail.com>
2019-09-25 17:35:47 +1200

    Added typed.js. Animated 'About Me' page top section.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import * as Typed from 'typed.js';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-about',
@@ -10,6 +12,28 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // How old am I?
+    const birthday = new Date('April 7, 1992');
+    const ageDifMs = +Date.now() - +birthday;
+    const ageDate = new Date(ageDifMs); // miliseconds from epoch
+    const ageInYears = (Math.abs(ageDate.getUTCFullYear() - 1970));
+
+    const commonAnimationSettings = {
+      typeSpeed: 20,
+      backSpeed: 50,
+      showCursor: true,
+      cursorChar: '|',
+      loop: false
+    };
+
+    const name = new Typed('#name', { ...commonAnimationSettings, strings: ['Sam Kavanagh'], startDelay: 0 }) ;
+    const age = new Typed('#age', { ...commonAnimationSettings, strings: [ageInYears.toString()], startDelay: 500 });
+    const nationality = new Typed('#nationality', { ...commonAnimationSettings, strings: ['New Zealand'], startDelay: 1000 });
+    const career = new Typed('#career', { ...commonAnimationSettings, strings: ['Nerd', 'Web Developer'], startDelay: 2000 });
+    const industry = new Typed('#years-in-industry', { ...commonAnimationSettings, strings: ['2'], startDelay: 3800 });
+    const education = new Typed('#education', { ...commonAnimationSettings, strings: ['PhD, Computer Science'], startDelay: 4500 });
+
   }
 
 }
