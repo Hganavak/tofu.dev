1e8f767b3cc37491a62aed5407ab693b70c44c08
Sam Kavanagh <hganavak@gmail.com>
2019-11-08 16:06:17 +1300

    Got route params for blog-page component

--- src/app/blog/blog-page/blog-page.component.ts
+++ src/app/blog/blog-page/blog-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog-page',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPageComponent implements OnInit {
 
-  constructor() { }
+  private blogPostSlug: string;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe( paramMap =>
+      this.blogPostSlug = paramMap.get('id')
+    );
   }
 
 }
