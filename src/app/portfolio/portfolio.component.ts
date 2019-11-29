beb2f5246e92c374e8a4acbbd57f3eb97575a6e2
Sam Kavanagh <hganavak@gmail.com>
2019-11-29 16:12:08 +1300

    Began work on Portfolio main page

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from '../contentful.service';
+import { Observable } from 'rxjs';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  portfolioItems$: Observable<any>;
+
+  constructor(
+    private contentfulService: ContentfulService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.portfolioItems$ = this.contentfulService.getEntries('portfolioItem');
   }
 
 }
