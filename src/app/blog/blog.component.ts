00423a0d23ee4b39aaa5f682852f66869580c594
Sam Kavanagh <hganavak@gmail.com>
2019-12-02 13:47:06 +1300

    Got portfolio item images to display

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -27,8 +27,8 @@ export class BlogComponent implements OnInit {
   ngOnInit() {
     // this.contentfulService.logContent('5qCY28J8NjC0XVIE4UU9HN');
     this.blogPosts$ = this.contentfulService.getEntries('blogPost');
-    this.blogPost$ = this.contentfulService.getContent('5qCY28J8NjC0XVIE4UU9HN');
-    this.indBlogPost$ = this.contentfulService.getBlogPostBySlug('5qCY28J8NjC0XVIE4UU9HN');
+    // this.blogPost$ = this.contentfulService.getContent('5qCY28J8NjC0XVIE4UU9HN');
+    // this.indBlogPost$ = this.contentfulService.getBlogPostBySlug('5qCY28J8NjC0XVIE4UU9HN');
 
 
     // console.log(this.blogPosts$[0]);
