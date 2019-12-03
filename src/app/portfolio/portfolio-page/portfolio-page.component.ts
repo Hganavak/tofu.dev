1041f663f5bf0f9b3ebc4df2bcd2dcf07d44248d
Sam Kavanagh <hganavak@gmail.com>
2019-12-03 17:09:47 +1300

    Started working on URL params for portfolio items

--- src/app/portfolio/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio/portfolio-page/portfolio-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { ContentfulService } from 'src/app/contentful.service';
 
 @Component({
   selector: 'app-portfolio-page',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioPageComponent implements OnInit {
 
-  constructor() { }
+  private portfolioItemSlug: string;
+  public portfolioItem$: Observable<any>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private contentfulService: ContentfulService
+  ) { }
 
   ngOnInit() {
+
+    this.route.paramMap.subscribe( paramMap =>
+      this.portfolioItemSlug = paramMap.get('id')
+    );
+
+    // this.portfolioItem$ = this.contentfulService.getPortfolioItemBySlug(this.portfolioItemSlug); // Implement
+
   }
 
 }
